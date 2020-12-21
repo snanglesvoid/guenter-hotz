65a263f33e97eeb1a1d334f5a1bf1ccf2744be65
Janik Hotz <janikhotz@gmail.com>
2020-12-21 17:41:10 +0100

    design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import {HttpClient} from '@angular/common/http'
 
 import {Observable} from 'rxjs'
 
-import {map, tap} from 'rxjs/operators'
+import {map, share, tap} from 'rxjs/operators'
 
 import {BibtexParser} from 'src/app/lib/Parser'
 import {BibtexEntry, compareEntries, trimFields} from './lib/BibtexEntry'
@@ -36,10 +36,35 @@ export class AppComponent implements OnInit {
       map(x => x.entries),
       map(x => x.sort(compareEntries)),
       map(x => x.map((y: BibtexEntry) => trimFields(y))),
+      share(),
+    )
+
+    this.bibByDecade = this.decades
+      .map(decade => this.getBibByDecade(decade))
+  }
+
+  public decades = [1950, 1960, 1970, 1980, 1990, 2000, 2010]
+    .map(x => ({start: x, end: x + 10}))
+    .reverse()
+
+  public bib$: Observable<BibtexEntry[]>
+
+  public getBibByDecade(decade: {start: number, end: number}): Observable<BibtexEntry[]> {
+    return this.bib$.pipe(
+      map(xs => xs.filter(x =>
+        +x.Fields.Year >= decade.start && +x.Fields.Year < decade.end,
+      )),
       tap(console.log),
     )
   }
 
-  public bib$: Observable<BibtexEntry[]>
+  public bibByDecade: Observable<BibtexEntry[]>[]
 
+  public decadeTitle(decade: any) {
+    if (decade.start === 2010) {
+      return '2010++'
+    } else {
+      return `${decade.start}–${decade.end}`
+    }
+  }
 }
